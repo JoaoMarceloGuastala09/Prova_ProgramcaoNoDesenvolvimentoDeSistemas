1875fd735b45c1a12ce31051d761d81a7bd01d7e
João Marcelo Guastala <guastala.joao@escola.pr.gov.br>
2026-04-15 09:21:52 -0300

    Add files via upload

--- TrianguloValido.py
+++ TrianguloValido.py
@@ -1,13 +1,19 @@
 import os
 from time import sleep
+
+historico = []
+sair = 0
+
 while True:
+    if sair == 1: break
+    else: pass
     while True:
         #Vamos embelezar esse codigo agora Denis
         print('=-'*15)
         print("Bem vindo!".center(30))
         print('=-'*15)
         try:
-            #Primeiro obtemos os valores de cada lado do traingulo
+            #Primeiro obtemos os valores de cada lado do triangulo
             v1 = int(input('Digite o primeiro valor: '))
             v2 = int(input('Digite o segundo valor: '))
             v3 = int(input('Digite o terceiro valor: '))
@@ -19,17 +25,37 @@ while True:
 
     #Faremos a verificação para decobrir se o triangulo é possivel
     if (v1+v2 > v3) and (v2+v3 > v1) and (v1+v3 > v2):
-        print("\033[32mÉ posivel formar um triangulo\033[0m")
+        res = 'POSSÍVEL'
+        print(f"\033[32mÉ {res} formar um triangulo\033[0m")
     else:
-        print("\033[31mNão é posivel formar um triangulo\033[0m")
+        res = 'IPOSSÍVEL    '
+        print(f"\033[31mE {res} formar um triangulo\033[0m")
+
+    historico.append(f'Lados: ({v1}, {v2}, {v3}) = Resultado: {res}')
     print('=-'*15)
     #Apos a verificação perguntamos se o usuario gostaria de tentar novamente
-    novamente = str(input("Deseja tentar novamente? \n[1] Sim\n[2] Não\nDigite sua respota:  ")).lower().strip()
-    if novamente == '1' or novamente == 's':
-        print("Boa consulta!")
-        sleep(2)
-        os.system('cls' if os.name == 'nt' else 'clear')
-    elif novamente == '2' or novamente =='n':
-        print("Obrigado por usar nosso sitema de verificação de triangulos. Até a próxima terraqueo.")
-        sleep(2)
-        break
+    while True:
+        novamente = str(input("Deseja tentar novamente? \n[1] Sim\n[2] Não\nDigite sua respota:  ")).lower().strip()
+        if novamente == '1' or novamente == 's':
+            print("Boa consulta!")
+            sleep(2)
+            os.system('cls' if os.name == 'nt' else 'clear')
+            break
+
+        elif novamente == '2' or novamente =='n':
+            print("\n" + "="*30)
+            print("HISTÓRICO DE CONSULTAS:")
+            for item in historico:
+                print(item)
+            print("="*30)
+            print("Obrigado por usar nosso sitema de verificação de triangulos. Até a próxima terraqueo.")
+            sleep(2)
+            sair = 1
+            break
+        
+        else:
+            print('\033[31mOpção INVALIDA tente novamente.\033[0m')
+            sleep(2)
+
+
+
